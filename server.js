9ea346982d95a77de7c6455ddb6fe49ca2c813bb
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-11-01 19:21:21 -0500

    seventh commit

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
